0d6caeea98a557577c348b309b368bd4835d1783
f.hanifah.hub <f.hanifah.ofc@gmail.com>
2021-01-05 15:43:18 +0700

    but first, vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="flex flex-col">
+      <div class="bg-black">
+        <h3>ini buat navbar</h3>
+      </div>
+      <div class="bg-gray-400 absolute top-0 right-0 h-16 w-16  ">
+        <div class="bg-gray-400 " style="width:400px; height:auto;">
+          <div class="bg-gray-300">
+            <h3>username</h3>
+          </div>
+          <div class="bg-gray-300">
+            <h3>password</h3>
+          </div>
+          <div class="bg-gray-300">
+          <h3>login form</h3>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
 };
 </script>
